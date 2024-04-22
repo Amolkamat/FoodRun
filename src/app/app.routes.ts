54d8ea8dc351175766169911e4819ee101c2500a
Danny Virasawmi <danv100@hotmail.com>
2024-04-22 19:56:38 -0400

    Completed Creditcard section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Burger25Component } from './components/burger25/burger25.component';
 import { BaconCheeseburgerComponent } from './components/burger25/bacon-cheeseburger/bacon-cheeseburger.component';
 import { SummaryComponent } from './components/burger25/summary/summary.component';
 import { CreditcardComponent } from './components/burger25/creditcard/creditcard.component';
+import { SuccessComponent } from './components/burger25/success/success.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -11,4 +12,5 @@ export const routes: Routes = [
   {path: 'burger25/bacon-cheeseburger', component: BaconCheeseburgerComponent},
   {path: 'burger25/summary', component: SummaryComponent},
   {path: 'burger25/creditcard', component: CreditcardComponent},
+  {path: 'burger25/success', component: SuccessComponent},
 ];
