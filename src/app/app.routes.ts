80562e1e4893dd2248a80b9b59f595833eacb4da
Danny Virasawmi <danv100@hotmail.com>
2024-04-22 19:31:58 -0400

    Summary completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { Burger25Component } from './components/burger25/burger25.component';
 import { BaconCheeseburgerComponent } from './components/burger25/bacon-cheeseburger/bacon-cheeseburger.component';
+import { SummaryComponent } from './components/burger25/summary/summary.component';
+import { CreditcardComponent } from './components/burger25/creditcard/creditcard.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'burger25', component: Burger25Component},
   {path: 'burger25/bacon-cheeseburger', component: BaconCheeseburgerComponent},
+  {path: 'burger25/summary', component: SummaryComponent},
+  {path: 'burger25/creditcard', component: CreditcardComponent},
 ];
---
+++ src/app/components/burger25/creditcard/creditcard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creditcard',
+  standalone: true,
+  imports: [],
+  templateUrl: './creditcard.component.html',
+  styleUrl: './creditcard.component.scss'
+})
+export class CreditcardComponent {
+
+}
