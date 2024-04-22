150e18087c8137bfe20072ba87e4f46af6ea448c
Danny Virasawmi <danv100@hotmail.com>
2024-04-22 18:05:13 -0400

    Burger 25 Main Menu completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { Burger25Component } from './components/burger25/burger25.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'burger25', component: Burger25Component},
 ];
