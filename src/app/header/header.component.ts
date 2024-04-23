9f1f1d32ce45f9f16e2f74baff9fec057bdef4b4
Danny Virasawmi <danv100@hotmail.com>
2024-04-22 22:50:56 -0400

    Finished Success page,  trying to get Links in header to work.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title = 'FoodRun';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  isLoginEnabled() {
+    if (this.router.url === "/" || "/burger25") {
+      console.log(this.router.url);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  isViewOrderEnabled() {
+    if (this.router.url === "/burger25/bacon-cheeseburger" || "/burger25/creditcard" || "/burger25/summary") {
+      console.log(this.router.url);
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
